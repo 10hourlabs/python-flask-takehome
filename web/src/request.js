0a7f7de8d11f27080965d960d9217b3ce9e6b628
dotunpeters <oyelakindotun@gmail.com>
2022-08-19 13:40:27 +0100

    update port

--- web/src/request.js
+++ web/src/request.js
@@ -6,7 +6,7 @@ export async function getAllCompanies() {
   try {
     let value = await axios({
       method: "get",
-      url: "http://127.0.0.1:3000/companies",
+      url: "http://127.0.0.1:3001/companies",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
     });
     return value.data.resource;
@@ -15,15 +15,15 @@ export async function getAllCompanies() {
   }
 }
 
-export async function getCleanersInCompany(company_id){
-    try{
-        let value = await axios({
-          method: "get",
-          url: `http://127.0.0.1:3000/users/${company_id}`,
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        });
-        return value.data.resource
-    }catch(error){
-        console.log('error', error)
-    }
+export async function getCleanersInCompany(company_id) {
+  try {
+    let value = await axios({
+      method: "get",
+      url: `http://127.0.0.1:3001/users/${company_id}`,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    });
+    return value.data.resource;
+  } catch (error) {
+    console.log("error", error);
+  }
 }
